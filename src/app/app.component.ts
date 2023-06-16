632a03884d4effbd2985732833b6845d576b93d8
Shirisha <shirisha.kengarla@theleanapps.com>
2023-06-16 23:28:15 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {  Component, OnInit, ViewChild } from '@angular/core';
+import { NgbPopover } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'popovers-app';
+export class AppComponent implements OnInit {
+  @ViewChild('myPopover') myPopover: NgbPopover;
+  @ViewChild('secondPopover') secondPopover:NgbPopover;
+  @ViewChild('thirdPopover') thirdPopover:NgbPopover;
+  ngOnInit() {
+    setTimeout(() => {
+      this.myPopover.open(); // Open the popover after a delay of 1 second
+    }, 1000);
+  }
+
+  openPopover() {
+    this.secondPopover.open();
+  }
+  closePopover() {
+    this.myPopover.close();
+  }
+  openSecondPopover(){
+    this.thirdPopover.open()
+  }
+  closeSecondPopover(){
+    this.secondPopover.close()
+  }
 }
